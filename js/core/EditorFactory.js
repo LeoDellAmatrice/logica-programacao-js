fc0f5f60f1a3d72fa051adc3912651f97011fa99
leonardo <Leonardo.dellamatrice@gmail.com>
2025-12-15 05:22:49 -0300

    melhorias editor

--- js/core/EditorFactory.js
+++ js/core/EditorFactory.js
@@ -1,3 +1,17 @@
+const PALAVRAS_PERMITIDAS = [
+  'console',
+  '.log',
+  'log',
+  'let',
+  'const',
+  'if',
+  'else',
+  'for',
+  'while',
+  'function',
+  'return'
+];
+
 export function EditorFactory() {
     let editor = null;
 
@@ -8,17 +22,59 @@ export function EditorFactory() {
         editor = CodeMirror(document.getElementById(elementId), {
             mode: "javascript",
             theme,
+            readOnly: false,
             lineNumbers: true,
             tabSize: 2,
             indentUnit: 2,
             smartIndent: true,
             matchBrackets: true,
+            autoCloseBrackets: true,
             autofocus: true,
+            styleActiveLine: true,
+            javascriptHint: true,
+            showHint: true,
+            extraKeys: {
+                'Ctrl-Space': 'autocomplete'
+            },
             value
         });
-
         editor.getWrapperElement().id = "codeMirrorEditor";
+
+        editor.on('inputRead', function (cm, change) {
+            if (
+                change.text[0] === '.' ||
+                /[a-zA-Z_]/.test(change.text[0])
+            ) {
+                cm.showHint({
+                    hint: hintEducacional,
+                    completeSingle: false
+                });
+            }
+        });
+
         return editor;
+    }
+
+    function hintEducacional(cm) {
+        const cursor = cm.getCursor();
+        const token = cm.getTokenAt(cursor);
+
+        const inicio = token.start;
+        const fim = cursor.ch;
+        const palavraAtual = token.string;
+
+        const sugestoes = PALAVRAS_PERMITIDAS
+            .filter(p => p.startsWith(palavraAtual))
+            .map(p => ({
+            text: p,
+            displayText: p
+            }));
+
+        return {
+            list: sugestoes,
+            from: CodeMirror.Pos(cursor.line, inicio),
+            to: CodeMirror.Pos(cursor.line, fim)
+        };
     }
 
     function getValue() {
