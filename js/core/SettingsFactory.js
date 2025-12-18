89fcd9425bb2e94441078afde3e8d98698273458
leonardo <Leonardo.dellamatrice@gmail.com>
2025-12-17 21:09:41 -0300

    remove console.log(settings)

--- js/core/SettingsFactory.js
+++ js/core/SettingsFactory.js
@@ -4,8 +4,6 @@ export function SettingsFactory(editor) {
   function applyAll() {
     const settings = settingsStore.getAll();
 
-    console.log(settings)
-
     editor.setTheme(settings.theme);
     editor.setFontSize(settings.fontSize);
     editor.setAutocomplete(settings.autocomplete);
